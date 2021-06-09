1592d8f0132e28efdba0d811c840ed9e3a044d46
Lucas-117 <williamsl23@greenhill.org>
2021-06-08 20:28:23 -0500

    Completed the challenge to the best of my ability

--- gamemenu.py
+++ gamemenu.py
@@ -1,51 +1,133 @@
-#game menu challenge 
-#Lucas Williams
+# game menu challenge 
+# Lucas Williams
+def gameover():
+    print("*"*21)
+    print("*"," "*1,"GAME COMPLETE"," "*1,"*")
+    print("*"*21)
+def score():
+    print("*"*28)
+    print("*"," "*5,"Lucas - 9999"," "*5,"*") # prints a scoreboard
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"," "*5,"Lucas - 9999"," "*5,"*")
+    print("*"*28)
 def menu():
     print("*"*28)
-    print("*"," "*6,"Forerunner"," "*6,"*")
+    print("*"," "*6,"Forerunner"," "*6,"*") # prints the main menu
     print("*"," "*9,"Menu"," "*9,"*")
     print("*"," "*24,"*")
-    print("*"," "*2,"L1- Two Betrayals"," "*3,"*")
-    print("*"," "*2,"L2- Sacred Icon"," "*5,"*")
-    print("*"," "*2,"L3- The Storm"," "*7,"*")
+    print("*"," "*2,"E1- Two Betrayals"," "*3,"*")
+    print("*"," "*2,"E2- Sacred Icon"," "*5,"*")
+    print("*"," "*2,"E3- The Storm"," "*7,"*")
+    print("*"," "*2,"S- List Scores"," "*6,"*")
     print("*"," "*2,"EX- Exit Game"," "*7,"*")
     print("*"*28)
-    print("Enter either L1,L2,L3,or EX", end= " ")
-menu() #calls the function
+    print("Enter either E1,E2,E3,S, or EX", end= " ")
+def gap():
+    print() # creates a space for a more pleasing U.I.
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+menu() # calls the function
 varChoice = str(input())
 while varChoice != "EX":
+    if varChoice  == "E1":
+        gap()
+        print("Welcome to the first encounter, in order to progress you must enter the missing number in the sequence,") # challenges the user with a math problem
+        print("5,8,13,21,__") # fibonacci
+        varAnswer = str(input())
+        if varAnswer == "34":
+            print("Congratulatious you may proceed to next encounter") # tests if the user answered the question correctly if not gives them two more chances at redemption
+            varChoice = "E2"
+        else:
+            print("Failure try again, you have two more chances.")# see earlier comment
+            varAnswer = str(input())
+            if varAnswer == "34":
+                print("Congratulatious you may proceed to next encounter")
+                varChoice = "E2"
+            else:
+                print("Failure try again, you have one more chance.")# see earlier comment
+                varAnswer = str(input())
+                if varAnswer == "34":
+                    print("Congratulatious you may proceed to next encounter")# see earlier comment
+                    varChoice = "E2"
+                else:
+                    print("Failure.")
+                    break
+
+    if varChoice  == "E2":
+        gap()
+        print("You have passed the Math Challenge and now the time has come to test your pop-culture knowledge") # tests if the user answered the question correctly if not gives them two more chances at redemption
+        print("Complete this phase, '_ _ _ shot first'")
+        varAnswer = str(input())
+        varAnswer = varAnswer.lower()
+        if varAnswer == "han":
+            print("You have passed this encounter, you may procede to the final enocunter.") # allows them passge to the final encounter by changing the value of varChoice
+            varChoice = "E3"
+        else:
+            print("Failure try again, you have two more chances.")
+            varAnswer = str(input())
+            if varAnswer == "han":
+                print("You have passed this encounter, you may procede to the final enocunter.")
+                varChoice = "E3"
+            else:
+                print("Failure try again, you have one more chance.")
+                varAnswer = str(input())
+                if varAnswer == "han":
+                    print("You have passed this encounter, you may procede to the final enocunter.")
+                    varChoice = "E3"
+                else:
+                    print("Failure.")
+                    break
+
+    if varChoice  == "E3":
+        gap()
+        print("You may have answered that question correctly, but tell me this,") # tests if the user answered the question correctly if not gives them two more chances at redemption
+        print("What walks on four legs in the morning, two legs during the day, and three in the evening?")
+        varAnswer = str(input())
+        varAnswer = varAnswer.lower()
+        if varAnswer == "man":
+            print()
+            print()
+            print()
+            print("Interesting, you have completed the trials, you may leave.") # decides the user has passed the trials
+            break
+
+        else:
+            print("Failure try again, you have two more chances.")
+            varAnswer = str(input())
+            if varAnswer == "man":
+                print()
+                print()
+                print()
+                print("Interesting, you have completed the trials, you may leave.")
+            else:
+                print("Failure try again, you have one more chance.")
+                varAnswer = str(input())
+                if varAnswer == "man":
+                    print()
+                    print()
+                    print()
+                    print("Interesting, you have completed the trials, you may leave.")
+                else:
+                    print("Failure.")
+                    break
+
+    if varChoice  == "S":
+        score()
     menu()
     varChoice = str(input())
-print("Goodbye, Have a nice day")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gameover()
 
 
 
